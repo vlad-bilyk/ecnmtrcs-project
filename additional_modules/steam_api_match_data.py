27958bc86171a5a70ad83fce17fd5aaf3181d9a1
vlad-bilyk <Qwertyu122>
2020-04-25 18:55:13 +0300

    documentation

--- additional_modules/steam_api_match_data.py
+++ additional_modules/steam_api_match_data.py
@@ -1,3 +1,5 @@
+# these functions were used in the first stages, but not anymore
+
 import requests
 import json
 
@@ -5,6 +7,10 @@ key = "0E51DE6A17DA1683650DC1EEACAD43DE"
 
 
 def get_specific_match_data(match_id):
+    """
+    :param match_id: id of a match
+    :return: dict - match data
+    """
     url = "http://api.steampowered.com/IDOTA2Match_570/GetMatchDetails/v1/?match_id={}&key={}".format(match_id, key)
     # print(url)
     json_data = requests.get(url).json()
@@ -13,6 +19,10 @@ def get_specific_match_data(match_id):
 
 
 def get_hero(hero_id):
+    """
+    :param hero_id: id of a hero
+    :return: str - name of a hero
+    """
     with open('data/heroes.json', 'r') as f:
         json_data = json.load(f)
     for i in json_data['result']['heroes']:
@@ -21,6 +31,10 @@ def get_hero(hero_id):
 
 
 def check_match_validity(match_id):
+    """
+    :param match_id: id of a match
+    return: dict - match data
+    """
     match_data = get_specific_match_data(match_id)
 
     if match_data['result']['duration'] < 1080:
